5dbf45f91051839263c741d68a0c3018968eea95
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-06-20 15:17:41 -0500

    Practicing How to use Devtools on Chrome

---
+++ console.js
@@ -0,0 +1,8 @@
+let firstNumber = 1;
+let seconfNumber = 2;
+
+let answer = firstNumber + seconfNumber;
+
+console.log(answer)
+console.log("The answer is", answer)
+console.log("The answer is " + answer)
